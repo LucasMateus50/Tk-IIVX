78fd77f6890b156fdeb9d5144fa76c9305e5c3fb
LucasMateus50 <101123260+LucasMateus50@users.noreply.github.com>
2022-03-11 14:37:25 -0300

    Update TK-IIVX-1.py

--- TK-IIVX-1.py
+++ TK-IIVX-1.py
@@ -4,7 +4,5 @@ from mpl_toolkits.mplot3d import axes3d
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 X, Y, Z = axes3d.get_test_data(0.02)
-ax.plot_wireframe(X, Y, Z,
-rstride=10,
-cstride=10)
+ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
 plt.show()
